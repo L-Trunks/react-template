0fd0b76a53d1ca4f29d0f26e1f807ef2cb15d0e2
liangzhengyu <18742509891@163.com>
2022-10-31 16:58:04 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import {
+    ConfigProvider
+} from 'antd'
+import zhCN from 'antd/es/locale/zh_CN'
+import { Provider } from 'react-redux'
+import store from './store'
+import './mock'
+//全局样式
+import '@/assets/style/base.less'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
+const antdConfig = {
+    locale: zhCN,
+}
+
+ReactDOM.render( 
+    <ConfigProvider {...antdConfig}>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </ConfigProvider>,
+    document.getElementById('root')
+)
